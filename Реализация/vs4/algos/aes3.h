bd291bbc785db6f99ed00a46ff1e9353ee02dfb6
daniel-momot <daniel.momot@gmail.com>
2020-06-13 08:22:33 +0300

    Intermediate commit

--- Реализация/vs4/algos/aes3.h
+++ Реализация/vs4/algos/aes3.h
@@ -48,183 +48,140 @@ const void* get_func(void* page) {
 
 	void* place = page;
 
-	const byte init[] = {
+	const byte code[] = {
 		0x60,									// pusha
-		//0x50, 0x53, 0x51, 0x52, 0x56, 0x57, 0x55, // push eax, ebx, ecx, edx, esi, edi, ebp // нов
 		0x31, 0xC9,								// xor    ecx,ecx
 		0xF7, 0xE1,								// mul    ecx
 		0x40,									// inc    eax
-		0xB1, 0x04, 							// mov    cl,0x4
+		0xB1, 0x04,								// mov    cl,0x4
 		0x60,									// pusha
 		0x8B, 0x74, 0x24, 0x44,					// mov    esi,[esp+0x44]
 		0x89, 0xE7,								// mov    edi,esp
 		0x60,									// pusha
 		0x01, 0xC9,								// add    ecx,ecx
-		0xF3, 0xA5,								// rep movsd
-		0x61									// popa
+		0xF3, 0xA5,								// rep movs
+		0x61,									// popa
+
+												// Multiplication over GF(2**8)
+
+		//0xE8, 0x10, 0x00, 0x00, 0x00,			// call   (***)
+		0xE8, 0x00, 0x00, 0x00, 0x00,			// call   0
+		0x5D,									// pop    ebp (***)
+		0xEB, 0x10,								// jmp   (***)
+
+		0x51,									// push   ecx
+		0xB1, 0x04,								// mov    cl,0x4
+		0x00, 0xC0,								// add    al,al (*)
+		0x73, 0x02,								// jae    (**)
+		0x34, 0x1B,								// xor    al,0x1b 
+		0xC1, 0xC8, 0x08,						// ror    eax,0x8 (**)
+		0xE2, 0xF5,								// loop   (*)
+		0x59,									// pop    ecx
+		0xC3,									// ret
+
+		//0x5D,									// pop    ebp (***)
+		0x83, 0xC5, 0x03,						// dec    ebp
+
+												// AddRoundKey, AddRoundConstant, ExpandRoundKey
+
+		0x60,									// pusha  (xxxx)
+		0x92,									// xchg   edx,eax
+		0x87, 0xFE,								// xchg   esi,edi
+		0x8B, 0x46, 0x1C,						// mov    eax,[esi+0x1c]
+		0xC1, 0xC8, 0x08,						// ror    eax,0x8
+		0x8B, 0x5E, 0x10,						// mov    ebx,[esi+0x10]  (|)
+		0x31, 0x1E,								// xor    [esi],ebx
+		0xA5,									// movs   
+		0xE8, 0xFC, 0xFF, 0xFF, 0xFF,			// call   ?
+		0xC1, 0xC8, 0x08,						// ror    eax,0x8
+		0xE2, 0xF0,								// loop   (|)
+		0x31, 0xD0,								// xor    eax,edx
+		0xB1, 0x04,								// mov    cl,0x4
+		0x31, 0x06,								// xor    [esi],eax (||)
+		0xAD,									// lods   
+		0xE2, 0xFB,								// loop   (||)
+		0x61,									// popa
+		0x3C, 0x6C,								// cmp    al,0x6c
+		0x0F, 0x85, 0x03, 0x00, 0x00, 0x00,		// jne    (x)
+		0x61,									// popa
+		0x61,									// popa
+		0xC3,									// ret
+
+		0xFF, 0xD5,								// call   ebp    (x)
+
+												// ShiftRows and SubBytes
+
+		0x60,									// pusha
+		0xB1, 0x10,								// mov    cl,0x10
+		0xAC,									// lods   (xx)
+		0xE8, 0xFC, 0xFF, 0xFF, 0xFF,			// call   ?
+		0x52,									// push   edx
+		0x89, 0xD3,								// mov    ebx,edx
+		0x83, 0xE3, 0x03,						// and    ebx,0x3
+		0xC1, 0xEA, 0x02,						// shr    edx,0x2
+		0x29, 0xDA,								// sub    edx,ebx
+		0x83, 0xE2, 0x03,						// and    edx,0x3
+		0x8D, 0x1C, 0x93,						// lea    ebx,[ebx+edx*4]
+		0x88, 0x04, 0x1F,						// mov    [edi+ebx*1],al
+		0x5A,									// pop    edx
+		0x42,									// inc    edx
+		0xE2, 0xE2,								// loop   (xx)
+		0x61,									// popa
+		0x3C, 0x6C,								// cmp    al,0x6c
+		0x74, 0xA5,								// je     (xxxx)
+
+												// MixColumns
+
+		0x60,									// pusha
+		0x8B, 0x07,								// mov    eax,[edi]   (yy)
+		0x89, 0xC3,								// mov    ebx,eax
+		0xC1, 0xC8, 0x08,						// ror    eax,0x8
+		0x89, 0xC2,								// mov    edx,eax
+		0x31, 0xD8,								// xor    eax,ebx
+		0xFF, 0xD5,								// call   ebp
+		0x31, 0xD0,								// xor    eax,edx
+		0xC1, 0xCB, 0x10,						// ror    ebx,0x10
+		0x31, 0xD8,								// xor    eax,ebx
+		0xC1, 0xCB, 0x08,						// ror    ebx,0x8
+		0x31, 0xD8,								// xor    eax,ebx
+		0xAB,									// stos   
+		0xE2, 0xE4,								// loop   (yy)
+		0x61,									// popa
+		0xEB, 0xDC,								// jmp    (xxxx)
+
+												// SubByte
+
+		0x60,									// pusha
+		0x84, 0xC0,								// test   al,al
+		0x74, 0x22,								// je     (kkk)
+		0x92,									// xchg   edx,eax
+		0xB1, 0xFF,								// mov    cl,0xff
+		0xB0, 0x01,								// mov    al,0x1   (zz)
+		0x84, 0xE4,								// test   ah,ah    (zzz)
+		0x75, 0x07,								// jne    (kkkk)
+		0x38, 0xD0,								// cmp    al,dl
+		0x0F, 0x94, 0xC4,						// sete   ah
+		0x74, 0xF3,								// je     (zz)
+		0x88, 0xC6,								// mov    dh,al  (kkkk)
+		0xFF, 0xD5,								// call   ebp
+		0x30, 0xF0,								// xor    al,dh
+		0xE2, 0xED,								// loop   (zzz)
+		0x88, 0xC2,								// mov    dl,al
+		0xB1, 0x04,								// mov    cl,0x4
+		0xD0, 0xC2,								// rol    dl,1   (zzzz)
+		0x30, 0xD0,								// xor    al,dl
+		0xE2, 0xFA,								// loop   (zzzz)
+		0x34, 0x63,								// xor    al,0x63  (kkk)
+		0x88, 0x44, 0x24, 0x1C,					// mov    [esp+0x1c],al
+		0x61,									// popa
+		0xC3									// ret
 	};
 
 
 
-	// AddRoundKey, AddRoundConstant, ExpandRoundKey
 
-	const byte b[] = {
-		0x60,								// pusha
-		0x92,								// xchg   edx,eax
-		0x87, 0xFE,							// xchg   esi,edi
-		0x8B, 0x46, 0x1C,					// mov    eax, [esi+0x1c]
-		0xC1, 0xC8, 0x08,					// ror    eax,0x8
-											//   <---------------------|
-		0x8B, 0x5E, 0x10,					// mov    ebx, [esi+0x10]  |
-		0x31, 0x1E,							// xor    [esi],ebx        |
-		0xA5,								// movsd                   |
-		0xFF, 0xD0,							// call   ?                |
-		0xC1, 0xC8, 0x08,					// ror    eax,0x8          |
-		0xE2, 0xF3,							// loop   -----------------|
-		0x31, 0xD0,							// xor    eax,edx
-		0xB1, 0x04,							// mov    cl,0x4
-											//   <---------------------|
-		0x31, 0x06,							// xor    [esi],eax        |
-		0xAD,								// lodsd                   |
-		0xE2, 0xFB,							// loop   -----------------|
-		0x61								// popa
-	};
-
-	const byte shift_rows[] = {
-		0x60,								// pusha
-		0xB1, 0x10,							// mov    cl,0x10
-											//   <---------------------|
-		0xAC,								// lods                    |
-		0xFF, 0xD0,							// call   ?
-
-		0x52,								// push   edx
-		0x89, 0xD3,							// mov    ebx,edx
-		0x83, 0xE3, 0x03,					// and    ebx,0x3
-		0xC1, 0xEA, 0x02,					// shr    edx,0x2
-		0x29, 0xDA,							// sub    edx,ebx
-		0x83, 0xE2, 0x03,					// and    edx,0x3
-		0x8D, 0x1C, 0x93,					// lea    ebx,[ebx+edx*4]
-		0x88, 0x04, 0x1F,					// mov    [edi+ebx*1],al
-		0x5A,								// pop    edx
-		0x42,								// inc    edx              |
-
-		0xE2, 0xE5,							// loop   -----------------|
-		0x61								// popa
-	};
-
-	const byte mix_columns[] = {
-		0x60,								// pusha
-		0x8B, 0x07,							// mov    eax,[edi]
-		0x89, 0xC3,							// mov    ebx,eax
-		0xC1, 0xC8, 0x08,					// ror    eax,0x8
-		0x89, 0xC2,							// mov    edx,eax
-		0x31, 0xD8,							// xor    eax,ebx
-		0xFF, 0xD5,							// call   ebp
-		0x31, 0xD0,							// xor    eax,edx
-		0xC1, 0xCB, 0x10,					// ror    ebx,0x10
-		0x31, 0xD8,							// xor    eax,ebx
-		0xC1, 0xCB, 0x08,					// ror    ebx,0x8
-		0x31, 0xD8,							// xor    eax,ebx
-		0xAB,								// stos
-		0xE2, 0xE4,							// loop   ?
-		0x61,								// popa
-		0xE9, 0xFC, 0xFF, 0xFF, 0xFF		// jmp    ?
-	};
-
-
-
-
-
-	//const byte fin[] = {
-	//	0x5D, 0x5F, 0x5E, 0x5A, 0x59, 0x5B, 0x58 // pop ebp, edi, esi, edx, ecx, ebx, eax
-	//};
-
-	//memcpy(place, init, sizeof(init));
-	//place = (byte*) place + sizeof(init);
-
-	//memcpy(place, mult, sizeof(mult));
-	//place = (byte*) place + sizeof(mult);
-
-	//memcpy(place, fin, sizeof(fin));
-	//place = (byte*) place + sizeof(fin);
-
-	//memcpy(place, sub_byte1, sizeof(sub_byte1));
-	//place = (byte*) place + sizeof(sub_byte1);
-
-	//memcpy(place, sub_byte2, sizeof(sub_byte2));
-	//place = (byte*) place + sizeof(sub_byte2);
-
-	//memcpy(place, sub_byte3, sizeof(sub_byte3));
-	//place = (byte*) place + sizeof(sub_byte3);
-
-	//memcpy(place, sub_byte4, sizeof(sub_byte4));
-	//place = (byte*) place + sizeof(sub_byte4);
-
-	//memcpy(place, sub_byte5, sizeof(sub_byte5));
-	//place = (byte*) place + sizeof(sub_byte5);
-
-	//memcpy(place, b1, sizeof(b1));
-	//place = (byte*) place + sizeof(b1);
-
-	//memcpy(place, b2, sizeof(b2));
-	//place = (byte*) place + sizeof(b2);
-
-	//memcpy(place, b3, sizeof(b3));
-	//place = (byte*) place + sizeof(b3);
-
-	memcpy(place, mix_columns, sizeof(mix_columns));
-	place = (byte*) place + sizeof(mix_columns);
-
-
-
-	const byte mult[] = {
-		//0xE8, 0x10, 0x00, 0x00, 0x00,	// call   15 <_main+0x15>
-		0x51,							// push   ecx
-		0xB1, 0x04,						// mov    cl,0x4
-		//0xB5, 0x00,						// mov ch, 0 // нов
-		0x00, 0xC0,						// add    al,al
-		0x73, 0x02,						// jae    $ + 4
-		0x34, 0x1B,						// xor    al,0x1b
-		0xC1, 0xC8, 0x08,				// ror    eax,0x8
-		0xE2, 0xF5,						// loop   $ - 9
-		0x59,							// pop    ecx
-		0xC3,							// ret
-		//0x5D							// pop    ebp
-	};
-
-	byte sub_byte[] = {
-		0x60,								// pusha
-		0x84, 0xC0,							// test   al,al
-		0x74, 0x34,							// je     sub_byte5
-		0x92,								// xchg   edx,eax
-		0xB1, 0xFF,							// mov    cl,0xff
-
-		0xB0, 0x01,							// mov    al,0x1
-		0x84, 0xE4,							// test   ah,ah
-		0x75, 0x07,							// jne    sub_byte3
-		0x38, 0xD0,							// cmp    al,dl
-		0x0F, 0x94, 0xC4,					// sete   ah
-		0x74, 0xF3,							// je     sub_byte2
-
-		0x88, 0xC6,							// mov    dh,al
-		0xFF, 0xD5,							// call   ebp
-		0x30, 0xF0,							// xor    al,dh
-		0xE2, 0xED,							// loop   sub_byte2 ( + 2)
-		0x88, 0xC2,							// mov    dl,al
-		0xB1, 0x04,							// mov    cl,0x4
-											// <--------------------|
-		0xD0, 0xC2,							// rol    dl,1          |
-		0x30, 0xD0,							// xor    al,dl         |
-		0xE2, 0xFA,							// loop ----------------|
-
-		0x34, 0x63,							// xor    al,0x63
-		0x88, 0x44, 0x24, 0x1C,				// mov    [esp+0x1c],al
-		0x61,								// popa
-		0xC3								// ret
-	};
-
-
-
+	memcpy(place, code, sizeof(code));
+	place = (byte*) place + sizeof(code);
 
 
 
@@ -235,12 +192,14 @@ const void* get_func(void* page) {
 #include "windows.h"
 
 void aes_asm (const byte plaintext[16], const byte key[16], byte cipher[16])  {
-	typedef byte* (*cryptofunc)(const byte*,const byte*);
+	//typedef byte* (*cryptofunc)(const byte*,const byte*);
+	typedef byte* (*cryptofunc)(void);
 
 	void* page = VirtualAlloc(NULL, 4096, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 	const cryptofunc func = (cryptofunc) get_func(page);
 
-	cipher = func(plaintext, key);
+	//cipher = func(plaintext, key);
+	cipher = func();
 
 	VirtualFree(page, 4096, MEM_RELEASE);
 }
